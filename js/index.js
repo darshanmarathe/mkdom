d0d02b9eca20489d01f7b386e7ad12b7147bb37a
darshanmarathe <darshanmarathe@gmail.com>
2023-10-26 17:58:35 +0530

    added clear remove and appendTo methods

--- js/index.js
+++ js/index.js
@@ -1,12 +1,59 @@
-
-function SayHi(){
-    alert('hello world');
-}
-
 const container = mkDOM('div' , {
     'id' : 'container',
     "class": 'container'
 })
+
+
+function SayHi(){
+    alert('hello world');
+  
+    const container2 = mkDOM('div' , {
+        'id' : 'container2',
+        "class": 'container'
+    })
+    
+    setTimeout(() => {
+        container2.clear().remove();
+    }, 5000);
+     container2.content("Append to test... will clear in 5 sec.")
+    .attr('data-id' , "container")
+    .append(
+        mkDOM('ul' , {'class' : 'menu'}).appendArray(
+            [1 , 2 , 3, 4 ,5] , function(i) {
+                return mkDOM('li' , {'class' : 'menuItem'})
+                .content(i)
+                .css('border' , '1px dashed green')
+                .css('text-align' , 'center')
+                .on('mouseenter' , function(){
+                    this.style.backgroundColor = 'gray';
+                })
+                .on('mouseout' , function(){
+                    this.style.backgroundColor = '';
+                })
+            }
+        )
+     )
+     .append(
+        mkDOM('button' , {
+            'type' : 'submit'
+         }).content('Submit')
+         .on('click' , "SayHi()")    
+    )
+    .append(
+        mkDOM('button' , {
+            'type' : 'submit'
+         }).content('reset with inline function')
+         .on('click' , function(e) {
+            e.preventDefault();
+            alert('resetting....')
+         })    
+    )
+
+
+
+    container2.AppendTo('#copy')
+
+}
 
 
 
--- js/mkDOM.js
+++ js/mkDOM.js
@@ -1,76 +1,75 @@
-var mkDOM = function(tag, props ={}){
-    if(tag === 'script' || tag === 'style') {
-        throw Error("Sorry script and style tags are not allowed");
-    }
-    const element =  document.createElement(tag);
-    Object.entries(props).forEach(([k ,v]) => {
-        element.setAttribute(k ,v)
-    })
-    return {
-        element,
-        markup: () => {
-            return element.outerHTML;
-        },
-        append(node) {
-            if(node.element){
-                element.appendChild(node.element);
-            }else{
+var mkDOM = function (tag, props = {}) {
+  if (tag === "script" || tag === "style") {
+    throw Error("Sorry script and style tags are not allowed");
+  }
+  const element = document.createElement(tag);
+  Object.entries(props).forEach(([k, v]) => {
+    element.setAttribute(k, v);
+  });
+  return {
+    element,
+    markup: () => {
+      return element.outerHTML;
+    },
+    append(node) {
+      if (node.element) {
+        element.appendChild(node.element);
+      } else {
+        element.appendChild(node);
+      }
+      return this;
+    },
+    data(key, value) {
+      if (!value) return element.dataset[key];
+      else element.dataset[key] = value;
+      return;
+    },
+    content: function (text) {
+      if (!text) return element.textContent;
+      else element.textContent = text;
 
-                element.appendChild(node);
-            }
-            return this;
-        },
-        data(key , value){
-            if(!value)
-            return element.dataset[key];
-            else
-            element.dataset[key] = value
-            return
-        },
-        content: function(text){
-           if(!text)
-            return element.textContent;
-           else 
-           element.textContent = text;
-        
-           return this;
-        },
-        attr(key , value){
-            if(!value)
-            return element.getAttribute(key)
-            else
-            element.setAttribute(key , value);
-            return this;
-        },
-        css(attr , value){
-            if(!value)
-            return element.style[attr] = value;
-            else
-            element.style[attr] = value;
-            return this;
-        },
-        on(event , func){
-            if(typeof func === 'string'){
-                element.setAttribute('on'+ event , func) 
-            }else{
+      return this;
+    },
+    attr(key, value) {
+      if (!value) return element.getAttribute(key);
+      else element.setAttribute(key, value);
+      return this;
+    },
+    css(attr, value) {
+      if (!value) return (element.style[attr] = value);
+      else element.style[attr] = value;
+      return this;
+    },
+    on(event, func) {
+      if (typeof func === "string") {
+        element.setAttribute("on" + event, func);
+      } else {
+        element["on" + event] = func;
+      }
+      return this;
+    },
+    appendArray(arr, func) {
+      arr.forEach((e) => this.append(func(e)));
+      return this;
+    },
+    clear() {
+      const children = Array.from(element.children);
+      children.forEach((e) => {
+        element.removeChild(e);
+      });
 
-                element['on'+ event] = func;
-            }
-            return this;
-        },
-        appendArray(arr , func){
-            arr.forEach(e => this.append(func(e)));
-            return this;
-        },
-        clear(){
-            element.childNodes.forEach(e => {
-                element.removeChild(e)
-            })
-            return this;
-        },
-        AppendTo(_selector){
-            document.querySelector(_selector).appendChild(element)
-        }
+      element.childNodes.forEach((e) => {
+        element.removeChild(e);
+      });
 
-    }
-}
+      return this;
+    },
+    remove() {
+      element.remove();
+    },
+    AppendTo(_selector) {
+      document.querySelector(_selector).appendChild(element);
+      return this;
+    },
+  };
+};
